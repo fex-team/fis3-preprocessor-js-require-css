b9389bc8c3fe1d1f41efdad8903c3cc07b2e2448
2betop <liaoxuezhi@icloud.com>
2016-01-01 12:32:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+fis3-preprocessor-js-require-css
+===============
+支持 js 中直接 require css. (es6 的 import 也支持，但是先通过 es6 => es5 的转换。)
+
+
+只能在 fis3 中使用。
+
+```
+npm install -g fis3-preprocessor-js-require-css
+```
+
+使用方式配置如下：
+
+```
+fis.match('*.{js,es,es6,jsx,ts,tsx}', {
+  preprocessor: fis.plugin('js-require-css')
+})
+```
+
+
+## 参数说明
+
+`mode`: 加载模式，默认为 `dependency`
+  * `dep | dependency` 【推荐】 简单的标记依赖，并将js语句中对应的 `require` 语句去除。fis 的资源加载程序能够分析到这块，并最终以 `<link>` 的方式加载 css.
+  * `embed | inline` 将目标 css 文件转换成 js 语句, 并直接内嵌在当前 js 中，替换原有 `require` 语句。
+  * `jsRequire` 将目标 css 文件转换成 js 语句，但是并不内嵌，而是产出一份同名的 js 文件，当前 `require` 语句替换成新产生的文件。
---
+++ index.js
@@ -0,0 +1,71 @@
+var lang = fis.compile.lang;
+var rRequire = /"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(\/\/[^\r\n\f]+|\/\*[\s\S]+?(?:\*\/|$))|\brequire\s*\(\s*('|")(.+?)\2\s*\)/g;
+var css2js = require('fis3-preprocessor-css2js');
+
+module.exports = function(content, file, options) {
+  var mode = options.mode;
+
+  return content.replace(rRequire, function(m, comment, quote, value) {
+    if (!value)return m;
+
+    var info = fis.project.lookup(value, file);
+
+    if (!info.file || !info.file.isCssLike) {
+      return m;
+    }
+
+    switch (mode) {
+      case 'dep':
+      case 'dependency':
+        m = "'" + lang.info.wrap(lang.require.wrap(value)) + "'";
+        break;
+
+
+      case 'embed':
+      case 'inline':
+        var f = info.file;
+        fis.compile(f);
+        var content = css2js.processCss(f.getContent(), {
+          template: 'vanilla_runner'
+        });
+        m = content;
+
+        break;
+
+      case 'jsRequire':
+        var f = info.file;
+        fis.compile(f);
+        var newFile = fis.file.wrap(f.dirname + '/' + f.filename + f.rExt + '.js');
+        var content = css2js.processCss(f.getContent(), {
+          template: 'vanilla_runner'
+        });
+        newFile.setContent(content);
+        newFile.isMod = true;
+        fis.compile(newFile);
+
+        file.extras = file.extras || {};
+        file.extras.derived = file.extras.derived || [];
+        file.extras.derived.push(newFile);
+
+        m = 'require(' + quote + (newFile.moduleId || newFile.id) + quote + ')'
+
+        break;
+
+      default:
+        break;
+    }
+
+    return m;
+  })
+};
+
+
+module.exports.defaultOptions = {
+  
+  // 可选择的值
+  // dep|dependency 【推荐】 简单的标记依赖，fis 资源加载器能把这些依赖分析到，然后直接在 <head> 中用 link 插入：
+  // embed | inline  像 webpack 那样直接把 css 转成 js 插入到 js 里面。
+  // jsRequire  将目标文件转成 js 但是并不内嵌，而是可以通过 js 的 require 方式加载。
+  mode: 'dep'
+
+}
